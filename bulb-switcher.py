521abcdd194e61a23efb04d12c24cdeeeda6d0e9
konstanteen <kostmetallist@gmail.com>
2019-11-20 16:25:52 +0300

    implemented much faster approach for bulb-switcher problem -- now it solves adaptively according to N input

--- bulb-switcher.py
+++ bulb-switcher.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
+from math import floor, sqrt
+
 class Solution:
     def getDivisorsNum(self, i: int) -> int: 
 
-        if i == 1: return 1;
-        # taking trivial solutions (1 and i)
-        div_num = 2
-        for j in range(2, i): 
+        div_num = 0
+        for j in range(1, floor(sqrt(i))+1): 
             if i%j == 0:
-                div_num += 1
+                if j*j == i:
+                    div_num += 1
+                else:
+                    div_num += 2
         return div_num
 
     def bulbSwitch(self, n: int) -> int: 
 
-        # odd number of divisors => bulb will be on
         on_bulbs_count = 0
-        for i in range(1, n+1):
-            if self.getDivisorsNum(i)%2:
-                on_bulbs_count += 1
+
+        # Depending on the number given as input, different
+        # solution approaches are used. First, as a slower
+        # technique (appr. O(n^(2/3))), is applied to 
+        # small numbers, for example, n < 100. Second, with
+        # linear complexity, is applied for bigger ns.
+        if (n < 100):
+            # idea: odd number of divisors => bulb will be on
+            for i in range(1, n+1):
+                if self.getDivisorsNum(i)%2:
+                    on_bulbs_count += 1
+        else:
+            # idea: the situation shows that only the bulbs with
+            # numbers being squares of 1, 2, 3, etc. will be
+            # turned on at the end of a process
+            squares = [i**2 for i in range(1, floor(sqrt(n))+1)]
+            on_bulbs_count = len(list(squares))
 
         return on_bulbs_count
+
+sol = Solution()
+print(sol.bulbSwitch(99999))
