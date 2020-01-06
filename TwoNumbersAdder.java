fd575d5fa9f981c4b336256a86bd31faca1b880d
kostmetallist <kostmetallist@gmail.com>
2020-01-06 21:25:07 +0300

    Add functionality for different lengths addition

--- TwoNumbersAdder.java
+++ TwoNumbersAdder.java
@@ -43,6 +43,35 @@ public class TwoNumbersAdder {
             n2 = n2.next;
         } while (n1 != null && n2 != null);
 
+        if (n1 == null && n2 != null) {
+            while (n2 != null) {
+                ListNode tmp = new ListNode(carry_value);
+                curr.next = tmp;
+                curr = tmp;
+                curr.val += n2.val;
+                carry_value = curr.val / 10;
+                curr.val %= 10;
+                n2 = n2.next;
+            }
+
+        } else if (n1 != null && n2 == null) {
+            while (n1 != null) {
+                ListNode tmp = new ListNode(carry_value);
+                curr.next = tmp;
+                curr = tmp;
+                curr.val += n1.val;
+                carry_value = curr.val / 10;
+                curr.val %= 10;
+                n1 = n1.next;
+            }
+
+        } else {
+            if (carry_value != 0) {
+                ListNode tmp = new ListNode(carry_value);
+                curr.next = tmp;
+            }
+        }
+
         return output;
     }
 
@@ -57,8 +86,10 @@ public class TwoNumbersAdder {
         ListNode l2 = new ListNode(5);
         ListNode l2_1 = new ListNode(6);
         ListNode l2_2 = new ListNode(4);
+        ListNode l2_3 = new ListNode(5);
         l2.next = l2_1;
         l2_1.next = l2_2;
+        l2_2.next = l2_3;
 
         System.out.println(l1);
         System.out.println(l2);
