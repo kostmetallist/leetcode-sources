9742fe59028bb8f43bdbbd0dc4f7b83016481d5e
kostmetallist <kostmetallist@gmail.com>
2020-01-06 20:48:59 +0300

    Add stub solution

---
+++ TwoNumbersAdder.java
@@ -0,0 +1,19 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) { 
+        val = x;
+    }
+}
+
+public class TwoNumbersAdder {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        return l1;
+    }
+
+    public static void main(String[] args) {
+        TwoNumbersAdder tna = new TwoNumbersAdder();
+    }
+}
