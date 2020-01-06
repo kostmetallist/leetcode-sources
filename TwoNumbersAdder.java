04f18d372693a7637cec1764b5139a55b56a7223
kostmetallist <kostmetallist@gmail.com>
2020-01-06 20:56:12 +0300

    Add toString implementation and basic main activities

--- TwoNumbersAdder.java
+++ TwoNumbersAdder.java
@@ -5,6 +5,17 @@ class ListNode {
     ListNode(int x) { 
         val = x;
     }
+
+    @Override
+    public String toString() {
+
+        String output = "( ";
+        ListNode curr = this;
+        while (curr != null) {
+            output += curr.val + " ";
+        }
+        return output + ")";
+    }
 }
 
 public class TwoNumbersAdder {
@@ -15,5 +26,17 @@ public class TwoNumbersAdder {
 
     public static void main(String[] args) {
         TwoNumbersAdder tna = new TwoNumbersAdder();
+        ListNode l1 = new ListNode(2);
+        ListNode l1_1 = new ListNode(4);
+        ListNode l1_2 = new ListNode(3);
+        l1.next = l1_1;
+        l1_1.next = l1_2;
+
+        ListNode l2 = new ListNode(5);
+        ListNode l2_1 = new ListNode(6);
+        ListNode l2_2 = new ListNode(4);
+        l2.next = l2_1;
+        l2_1.next = l2_2;
+        tna.addTwoNumbers(l1, l2);
     }
 }
