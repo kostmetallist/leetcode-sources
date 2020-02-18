df14ba8c7352a5b95f3b38c4c69100781bd0c5b9
kostmetallist <kostmetallist@gmail.com>
2020-02-19 01:30:17 +0300

    Provide O(n) solution using extra 2n memory

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -6,27 +6,31 @@ from typing import List
 class Solution:
     def check_ability_to_store(self, height: List[int]) -> bool:
         # returning zero volume in case we have less than two blocks: 
-        # valid reservoir can be constructed with at least 3 pillars
+        # valid reservoir can be constructed with at least 3 verticals
         if len(height) < 2:
             return False
         return True
-
-    @staticmethod
-    def calculate_pool_volume(height: List[int], fr: int) -> int:
-        assert fr >= 0, 'Index is not a valid value'
-        return 0
 
     def trap(self, height: List[int]) -> int:
         if not self.check_ability_to_store(height): 
             return 0
 
-        pivot_end  = 0
-        second_end = 1
-        while second_end < len(height):
-            second_end += 1
-            pass
-        Solution.calculate_pool_volume(height, 0)
-        return 0
+        L      = len(height)
+        result = 0
+        left_max  = [height[0] for i in range(L)]
+        right_max = [height[L-1] for i in range(L)]
+
+        for i in range(1, L-1):
+            left_max[i]  = max(left_max[i-1], height[i])
+            right_max[L-i-1] = max(right_max[L-i], height[L-i-1])
+
+        for i in range(1, L-1):
+            result += min(left_max[i], right_max[i]) - height[i]
+
+        return result
+
 
 sol = Solution()
-sol.trap([4, 8, 15, 16, 23, 42])
+print(sol.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+print(sol.trap([1, 1, 1, 1]))
+print(sol.trap([1, 0, 0, 1]))
